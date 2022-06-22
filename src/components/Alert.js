0d5f5c2e14cac110038b78d1be3d3d6b2c27f661
MarcoCiau <marcoantoniociau@hotmail.com>
2022-06-22 15:31:35 -0500

    display alert based on state from redux

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,5 +1,7 @@
+import { useSelector } from "react-redux";
 const Alert = () => {
-  return <div className="alert alert-danger">alert goes here</div>;
+  const { alertType, alertText } = useSelector((state) => state.ui); //get events from redux's store
+  return <div className={`alert alert-${alertType}`}>{alertText}</div>
 };
 
 export default Alert;
