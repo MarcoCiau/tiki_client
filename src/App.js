fa767974c67f1cce13297d7a0787107623d7b118
MarcoCiau <marcoantoniociau@hotmail.com>
2022-06-22 11:06:51 -0500

    utilize app router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
+import { AppRouter } from "./router/AppRouter";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>Test</p>
-      </header>
+      <AppRouter />
     </div>
   );
 }
