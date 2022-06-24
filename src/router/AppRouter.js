6293510a1c03e55ca6186918a7f7b5f8eaf83334
MarcoCiau <marcoantoniociau@hotmail.com>
2022-06-24 16:49:10 -0500

    aplying routes for dashboard pages

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -1,27 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Error, Landing, Register } from "../pages/index";
 import {
+  AddJob,
+  AllJobs,
+  Profile,
   SharedLayout,
+  Stats,
 } from "../pages/dashboard";
 import ProtectedRoute from "./ProtectedRoute";
 export const AppRouter = () => {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route 
+        <Route
           path="/"
           element={
             <ProtectedRoute>
               <SharedLayout />
             </ProtectedRoute>
           }
-        />
-          {/* <Route index element={<Stats />} />
+        >
+          <Route index element={<Stats />} />
           <Route path="all-jobs" element={<AllJobs />} />
           <Route path="add-job" element={<AddJob />} />
           <Route path="profile" element={<Profile />} />
-        </Route> */}
+        </Route>
         <Route path="/register" element={<Register />} />
         <Route path="/landing" element={<Landing />} />
         <Route path="*" element={<Error />} />
