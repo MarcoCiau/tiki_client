748b6bc538975179e2bf4765b107dbf00d50ed29
MarcoCiau <marcoantoniociau@hotmail.com>
2022-06-24 14:50:14 -0500

    deprecated componen

--- src/router/PrivateRouter.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-// import { useSelector } from "react-redux";//TODO: implement state
-import { Navigate } from "react-router-dom";
-
-export const PrivateRoute = ({ children }) => {
-  const {uid} = useSelector(state => state.auth);//TODO: handle state
-  return true ? children : <Navigate to="/login" />;
-};
