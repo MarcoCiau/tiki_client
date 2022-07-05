39ea721cc3d220c2e86ee904c3680dec716bc410
MarcoCiau <marcoantoniociau@hotmail.com>
2022-07-05 17:45:35 -0500

    devices page WIP

--- src/pages/dashboard/Device.js
+++ src/pages/dashboard/Device.js
@@ -1,9 +1,40 @@
-import React from 'react'
-
+import React, { useMemo } from "react";
+import makeData from "../../util/makeData";
+import { Table } from "../../components";
+import TableActions from "../../components/TableActions";
 const Device = () => {
-  return (
-    <div>Device</div>
-  )
-}
+  const columns = React.useMemo(
+    () => [
+      {
+        Header: "Name",
+        accessor: "name",
+      },
+      {
+        Header: "MAC Address",
+        accessor: "mac",
+      },
+      {
+        Header: "Connected",
+        accessor: "connected",
+      },
+      {
+        Header: "Last Disconnection",
+        accessor: "lastConnected",
+      },
+      {
+        Header: "Last Report",
+        accessor: "lastReport",
+      },
+      {
+        Header: "Actions",
+        accessor: "actions",
+        Cell: ({ cell: { value } }) => <TableActions values={value} />,
+      },
+    ],
+    []
+  );
+  const data = React.useMemo(() => makeData(20), []);
+  return <Table columns={columns} data={data} />;
+};
 
-export default Device
+export default Device;
