03420b60b5ab87b281b0ce98bf729debf7134130
MarcoCiau <marcoantoniociau@hotmail.com>
2022-07-06 15:04:48 -0500

    deprecated styles

--- src/assets/wrappers/PageActions.js
+++ src/assets/wrappers/PageActions.js
@@ -11,17 +11,5 @@ const Wrapper = styled.main`
   .action 
     margin-bottom: 1rem;
   }
-  .icon:hover svg {
-      color: var(--primary-500);
-    }
-  .icon {
-    font-size: 1.5rem;
-    margin-right: 1rem;
-    display: flex;
-    align-items: center;
-    svg {
-      color: var(--grey-400);
-    }
-  }
 `;
 export default Wrapper;
