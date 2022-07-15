fecc5a8237d098f246451328a47a1303ece904a9
MarcoCiau <marcoantoniociau@hotmail.com>
2022-07-15 13:03:09 -0500

    missed files

---
+++ src/assets/wrappers/StatsContainer.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.section`
+align-items: center;
+display: flex;
+flex-direction: row;
+flex-wrap:wrap;
+justify-content: center;
+/* justify-content: space-evenly; */
+  /* display: grid;
+  row-gap: 2rem;
+  @media (min-width: 768px) {
+    grid-template-columns: 1fr 1fr;
+    column-gap: 1rem;
+  }
+  @media (min-width: 1120px) {
+    grid-template-columns: 1fr 1fr 1fr;
+    column-gap: 1rem;
+  } */
+`
+export default Wrapper
