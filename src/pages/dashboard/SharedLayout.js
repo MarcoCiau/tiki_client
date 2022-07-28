5ec8a9d8d796d8f9a292e08ade52881e66256b6e
MarcoCiau <marcoantoniociau@hotmail.com>
2022-07-28 15:43:56 -0500

    fetching device list

--- src/pages/dashboard/SharedLayout.js
+++ src/pages/dashboard/SharedLayout.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Wrapper from "../../assets/wrappers/SharedLayout";
 import { BigSidebar, Navbar, SmallSidebar } from "../../components";
+import { getDevices } from "../../context/actions";
+import { useAppContext } from "../../context/appContext";
 const SharedLayout = () => {
+  const { dispatch, totalDevices,
+  } = useAppContext();
+
+  useEffect(() => {
+    getDevices(dispatch);
+  }, [totalDevices]);
+  
   return (
     <Wrapper>
       <main className="dashboard">
