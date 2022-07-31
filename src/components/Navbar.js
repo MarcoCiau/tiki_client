c46c12027e31b2567321e76e2f02ddf4d1690ad8
MarcoCiau <marcoantoniociau@hotmail.com>
2022-07-31 11:50:21 -0500

    removing logo and text

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,8 +16,6 @@ const Navbar = () => {
         </button>
 
         <div>
-          <Logo />
-          <h3 className="logo-text">dashboard</h3>
         </div>
 
         <div className="btn-container">
