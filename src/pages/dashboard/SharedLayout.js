284ab0b535231636390343d4ec9f2e24aee7804e
MarcoCiau <marcoantoniociau@hotmail.com>
2022-07-31 15:03:42 -0500

    removing useEffect

--- src/pages/dashboard/SharedLayout.js
+++ src/pages/dashboard/SharedLayout.js
@@ -1,17 +1,8 @@
-import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Wrapper from "../../assets/wrappers/SharedLayout";
 import { BigSidebar, Navbar, SmallSidebar } from "../../components";
-import { getDevices } from "../../context/actions";
-import { useAppContext } from "../../context/appContext";
-const SharedLayout = () => {
-  const { dispatch, totalDevices,
-  } = useAppContext();
 
-  useEffect(() => {
-    getDevices(dispatch);
-  }, [totalDevices]);
-  
+const SharedLayout = () => {  
   return (
     <Wrapper>
       <main className="dashboard">
